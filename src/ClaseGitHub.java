2b61dacb134beb5dcfb21695c22a30d63eedf96e
Martín <Martn@DESKTOP-GMU97KT>
2019-01-16 10:58:32 +0100

    Clase Ejecutable

---
+++ src/ClaseGitHub.java
@@ -0,0 +1,9 @@
+
+public class ClaseGitHub {
+
+	public static void main(String[] args) {
+		System.out.println("Primer repositorio para github");
+		System.out.println("Creado por Martin Londres Blanes");
+	}
+
+}
