f83dbbade14cf451049d550fa10c604dcb3337eb
21856608 <21856608@N205-07.alumnos.uem.es>
2019-01-16 11:28:55 +0100

    cambio

--- src/ClaseGitHub.java
+++ src/ClaseGitHub.java
@@ -5,6 +5,7 @@ public class ClaseGitHub {
 		System.out.println("Primer repositorio para github");
 		System.out.println("Creado por Martin Londres Blanes");
 		System.out.println("Cambio realizado por Ricardo");
+		System.out.println("Cambio realizado por Juan");// J.J
 		
 		String dormi = "Qqué ganas tengo ";
 		String mas = "de echarme una buena siesta!";
