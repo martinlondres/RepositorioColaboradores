6ae8adf02a9f85682ca6e45b424a097aaccddd66
21856675 <21856675@N205-13.alumnos.uem.es>
2019-01-16 11:03:53 +0100

    Cambio Ricardo

--- src/ClaseGitHub.java
+++ src/ClaseGitHub.java
@@ -4,6 +4,7 @@ public class ClaseGitHub {
 	public static void main(String[] args) {
 		System.out.println("Primer repositorio para github");
 		System.out.println("Creado por Martin Londres Blanes");
+		System.out.println("Cambio realizado por Ricardo");
 	}
 
 }
